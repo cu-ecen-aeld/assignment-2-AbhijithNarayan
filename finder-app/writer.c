161a19316b3a8d400dedb7330c39f98da5b1268d
Abhijithnarayan <abhijithjith5@gmail.com>
2023-11-01 18:12:28 +0530

    second

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -9,7 +9,7 @@ int main(int argc, char* argv[]) {
     if (argc != 3) {
         syslog(LOG_ERR, "Error: Insufficient arguments. Usage: %s <writefile> <writestr>", argv[0]);
         closelog();
-        exit(1);
+        return 1;
     }
 
     char* writefile = argv[1];
@@ -19,14 +19,14 @@ int main(int argc, char* argv[]) {
     if (strlen(writefile) == 0 || strlen(writestr) == 0) {
         syslog(LOG_ERR, "Error: Empty arguments are not allowed.");
         closelog();
-        exit(1);
+        return 1;
     }
 
     FILE* file = fopen(writefile, "w");
     if (file == NULL) {
         syslog(LOG_ERR, "Error: Unable to open file for writing.");
         closelog();
-        exit(1);
+        return 1;
     }
 
     // Write the string to the file
